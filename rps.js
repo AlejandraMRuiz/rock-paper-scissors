94100a16a5838fa2256f41717596aa51d662aa94
AlejandraMRuiz <kovydcode@gmail.com>
2020-11-20 19:29:13 -0800

    background img + home page text content

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-// first commit
+
 
 
 
@@ -10,28 +10,32 @@
 // PROJECT PLAN:
 
 // STEP 0: Add a background pic.
-// 
-
-// STEP 1: Add header.
 // DONE
 
-// STEP 2: Add clickable 3 pics (R, P & S) that show hand on hover.
+// STEP 1: Add Home page text content.
+// DONE
+
+// STEP 2: Add a translucent background to Home page text.
 // 
 
-// STEP 3: Have computer display outcome based on what was clicked (if statement).
+// STEP 3: Link to game page.
 
-// STEP 4: Have score updated based on displayed outcome.
+// STEP 4: Add clickable 3 pics (R, P & S) that show hand on hover.
 
-// STEP 5: If it's a tie, add 1 point to BOTH sides.
+// STEP 5: Have computer display outcome based on what was clicked (if statement).
 
-// STEP 6: On loading, nice pop-up explaining the history of the game.
+// STEP 6: Have score updated based on displayed outcome.
 
-// STEP 7: Add HTML such as "player", "computer", etc.
+// STEP 7: If it's a tie, add 1 point to BOTH sides.
 
-// STEP 8: Fix any loading delays.
+// STEP 8: On loading, nice pop-up explaining the history of the game.
 
-// STEP 9: Fix up design.
+// STEP 9: Add HTML such as "player", "computer", etc.
 
-// STEP 10: Tidy up code. Clean, refactor.
+// STEP 10: Fix any loading delays.
 
-// STEP 11: Submit.
+// STEP 11: Fix up design.
+
+// STEP 12: Tidy up code. Clean, refactor.
+
+// STEP 13: Submit.
