fba687163972c524eb35ae4ec0a631e0025f4ada
AlejandraMRuiz <kovydcode@gmail.com>
2020-11-20 16:50:26 -0800

    background image sos

--- rps.js
+++ rps.js
@@ -1,1 +1,37 @@
 // first commit
+
+
+
+
+
+
+
+
+// PROJECT PLAN:
+
+// STEP 0: Add a background pic.
+// 
+
+// STEP 1: Add header.
+// DONE
+
+// STEP 2: Add clickable 3 pics (R, P & S) that show hand on hover.
+// 
+
+// STEP 3: Have computer display outcome based on what was clicked (if statement).
+
+// STEP 4: Have score updated based on displayed outcome.
+
+// STEP 5: If it's a tie, add 1 point to BOTH sides.
+
+// STEP 6: On loading, nice pop-up explaining the history of the game.
+
+// STEP 7: Add HTML such as "player", "computer", etc.
+
+// STEP 8: Fix any loading delays.
+
+// STEP 9: Fix up design.
+
+// STEP 10: Tidy up code. Clean, refactor.
+
+// STEP 11: Submit.
