de8cdb421431d376d925b473bcac808821514454
AlejandraMRuiz <kovydcode@gmail.com>
2020-11-22 02:28:21 -0800

    gotta end the night somewhere

--- rps.js
+++ rps.js
@@ -1,4 +1,58 @@
 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+
+rock.addEventListener('click', () => {
+    renderResults('rock');
+});
+
+
+paper.addEventListener('click', () => {
+    renderResults('paper');
+});
+
+
+scissors.addEventListener('click', () => {
+    renderResults('scissors');
+});
+
+
+function renderResults(userWeapon) {
+    const userInput = userWeapon;
+    const cpuWeapon = generateRandomSelection();
+    if (userInput === 'rock' && cpuWeapon === 'paper') {
+        win();
+    // add a ternary in win() function outside of renderResults(). 
+    // should say s/g like: IF userInput/Weapon === 'rock', display 'You win! Rock crushes Scissors!'
+    // ELSE: 'You lose... Paper covers Rock :(' 
+    } else if (userInput === 'rock' && cpuWeapon === 'scissors') {
+        loss();
+    } else if (userInput === 'paper' && cpuWeapon === 'scissors') {
+        win();
+    } else if (userInput === 'paper' && cpuWeapon === 'rock') {
+        loss();
+    } else if (userInput === 'scissors' && cpuWeapon === 'rock') {
+        win();
+    } else if (userInput === 'scissors' && cpuWeapon === 'paper') {
+        loss();
+    } else {
+        tie();
+    }
+};
+
+function generateRandomSelection()  {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    const weaponsArray = ['rock', 'paper', 'scissors'];
+    const cpuWeapon = weaponsArray[randomNumber - 1];
+
+
+
+
+};
+
+
 
 
 
@@ -15,15 +69,17 @@
 // STEP 1: Add Home page text content.
 // DONE
 
-// STEP 2: Add a translucent background to Home page text.
-// in progress...
+// STEP 2: Make Home page text readable.
+// DONE
 
 // STEP 3: Link to game page.
 // DONE
 
 // STEP 4: Add clickable 3 pics (R, P & S) that show hand on hover.
+// DONE
 
 // STEP 5: Have computer display outcome based on what was clicked (if statement).
+// in progress...
 
 // STEP 6: Have score updated based on displayed outcome.
 
