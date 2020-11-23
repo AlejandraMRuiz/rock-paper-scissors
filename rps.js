69b33212c3885f3bfc7b685f9ce442a1d2ff3f1e
AlejandraMRuiz <kovydcode@gmail.com>
2020-11-22 23:39:14 -0800

    uncaught reference error sos

--- rps.js
+++ rps.js
@@ -1,56 +1,97 @@
 
-const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
 
-rock.addEventListener('click', () => {
-    renderResults('rock');
+window.addEventListener("DOMContentLoaded", () => {
+    const timeFormatBtn = document.getElementById("button");
+    timeFormatBtn.addEventListener("click", function(event) {
+      event.stopPropagation();
+      is24HourTime = !is24HourTime;
+    });  
+  });
+
+
+// commented code (rps.js:47-57) produces...
+// 'Uncaught ReferenceError: results not defined at renderResults (rps.js:83)
+// at HTMLImageElement.<annonymous> (rps.js:42)'
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const rock = document.getElementById('rock');
+    rock.addEventListener('click', function(event) {
+        event.stopPropagation();
+        renderResults('rock');
+    });
 });
 
 
-paper.addEventListener('click', () => {
-    renderResults('paper');
+window.addEventListener('DOMContentLoaded', () => {
+    const paper = document.getElementById('paper');
+    paper.addEventListener('click', function(event) {
+        event.stopPropagation();
+        renderResults('paper');
+    });
 });
 
 
-scissors.addEventListener('click', () => {
-    renderResults('scissors');
+window.addEventListener('DOMContentLoaded', () => {
+    const scissors = document.getElementById('scissors');
+    scissors.addEventListener('click', function(event) {
+        event.stopPropagation();
+        renderResults('scissors');
+    });
 });
+
+
+// rock.addEventListener('click', () => {
+//     renderResults('rock');
+// });
+
+// paper.addEventListener('click', () => {
+//     renderResults('paper');
+// });
+
+// scissors.addEventListener('click', () => {
+//     renderResults('scissors');
+// });
 
 
 function renderResults(userWeapon) {
     const userInput = userWeapon;
-    const cpuWeapon = generateRandomSelection();
+    const cpuWeapon = generateRandomWeapon();
+
     if (userInput === 'rock' && cpuWeapon === 'paper') {
-        win();
-    // add a ternary in win() function outside of renderResults(). 
-    // should say s/g like: IF userInput/Weapon === 'rock', display 'You win! Rock crushes Scissors!'
-    // ELSE: 'You lose... Paper covers Rock :(' 
+        results-display.textContent === 'You lose :( Paper covers Rock...';
+
     } else if (userInput === 'rock' && cpuWeapon === 'scissors') {
-        loss();
+        results-display.textContent === 'You win! Rock crushes Scissors!';
+
     } else if (userInput === 'paper' && cpuWeapon === 'scissors') {
-        win();
+        results-display.textContent === 'You lose :( Scissors cuts Paper...';
+
     } else if (userInput === 'paper' && cpuWeapon === 'rock') {
-        loss();
+        results-display.textContent === 'You win! Paper covers Rock!';
+
     } else if (userInput === 'scissors' && cpuWeapon === 'rock') {
-        win();
+        results-display.textContent === 'You lose :( Rock crushes Scissors...';
+
     } else if (userInput === 'scissors' && cpuWeapon === 'paper') {
-        loss();
+        results-display.textContent === 'You win! Scissors cuts Paper!';
+
     } else {
-        tie();
+        results-display.textContent === 'You tied! One point for each side :)';
     }
 };
 
-function generateRandomSelection()  {
+
+function generateRandomWeapon()  {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     const weaponsArray = ['rock', 'paper', 'scissors'];
     const cpuWeapon = weaponsArray[randomNumber - 1];
-
-
-
-
+    return cpuWeapon;
 };
+
 
 
 
@@ -78,8 +119,11 @@ function generateRandomSelection()  {
 // STEP 4: Add clickable 3 pics (R, P & S) that show hand on hover.
 // DONE
 
+
 // STEP 5: Have computer display outcome based on what was clicked (if statement).
 // in progress...
+// you should probably break this target down into its parts.
+
 
 // STEP 6: Have score updated based on displayed outcome.
 
